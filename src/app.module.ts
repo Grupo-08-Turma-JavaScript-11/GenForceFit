25ff2d43edc91330ffdee597c8c7d49a6b621e1c
wesleygf <wesleygfsilva@hotmail.com>
2025-12-17 11:31:19 -0300

    pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "root",
+    password: "12345678",
     database: "db_genforcefit",
     entities: [],
     synchronize: true,
