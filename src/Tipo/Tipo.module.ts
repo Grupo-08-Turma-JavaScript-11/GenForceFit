a48df3a36703f2cb6ea097cf746c26334171ee80
Juan G <juangustavo0508@gmail.com>
2025-12-17 19:40:42 -0300

    Fix das pastas

--- src/Tipo/Tipo.module.ts
+++ src/Tipo/Tipo.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Tipo } from './entities/tipo.entity'
 import { TipoService } from './service/tipo.service'
-import { TipoController } from './controller/tipo.controller'
+import { TipoController } from './Controller/tipo.controller'
+
+
 
 
 @Module({
