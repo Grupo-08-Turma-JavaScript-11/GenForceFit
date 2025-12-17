f15a9071ae036cdc2ec8ee2af79d5fbfcf79e868
wesleygf <wesleygfsilva@hotmail.com>
2025-12-17 20:45:57 -0300

    Organizando sistema de arquivos

---
+++ src/tipo/entities/tipo.entity.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Exercicio } from "../../exercicio/entities/exercicio.entity";
+
+
+@Entity({ name: "tb_tipo" })
+export class Tipo {
+
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty()
+  descricao: string;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  @ApiProperty()
+  grupo_muscular: string;
+
+  @OneToMany(() => Exercicio, (exercicio) => exercicio.tipo)
+  @ApiProperty()
+  exercicio: Exercicio[];
+}
